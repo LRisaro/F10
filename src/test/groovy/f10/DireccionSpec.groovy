a81d9dfe908d8138e5e6e4eb8dcf64143e93770f
Javier <javier.nunezleyes@gmail.com>
2022-06-13 19:28:06 -0300

    Base test

--- src/test/groovy/f10/DireccionSpec.groovy
+++ src/test/groovy/f10/DireccionSpec.groovy
@@ -13,9 +13,13 @@ class DireccionSpec extends Specification implements DomainUnitTest<Direccion> {
 
     void "CrearNuevaDireccion"() {
 
+        given:"Como propietario de un establecimiento"
+
+        when:"Creo mi establecimiento"
         Direccion direccion = new Direccion(calle: "Suipacha", altura: "875");
-        expect:"fix me"
-            direccion.calle == "Suipacha";
-            direccion.altura == "875";
+
+        then:"Verifco que los datos son correctos"
+        direccion.calle == "Suipacha";
+        direccion.altura == "875";
     }
 }
