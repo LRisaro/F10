75808a07c6c721f12a9f60e9cd6d0299b2db6cf1
Alexis Daciuk <adaciuk@gmail.com>
2022-06-15 22:20:34 -0300

    Agrego test para US5 (#7)
    * WIP
    
    * Agrego codigo necesario para los test de integracion del US5

---
+++ src/integration-test/groovy/f10/US5AltaJugadorPartido.groovy
@@ -0,0 +1,73 @@
+package f10
+
+import grails.testing.mixin.integration.Integration
+import grails.gorm.transactions.*
+import spock.lang.Specification
+
+@Integration
+class US5AltaJugadorPartido extends Specification {
+
+  def setup() {
+  }
+
+  def cleanup() {
+  }
+
+  void AltaJugadorSinPenalizacionesAPartido() {
+    given:'Como jugador de un grupo de amigos quiero anotarme para jugar un partido para participar del mismo.'
+    GrupoDeAmigos grupo = new GrupoDeAmigos(nombre:'F10')
+    Jugador jugador = new Jugador()
+    grupo.agregarAmigo(jugador)
+    Cancha cancha = new Cancha(numero: 5)
+    Partido partido = new Casual(new Date(), cancha)
+
+    when:'Me anoto y confirmo'
+    partido.anotarJugador(jugador)
+    partido.confirmarJugador(jugador)
+
+    then:'Quedo como jugador anotado y confirmado en el partido'
+    partido.jugadoresAnotados.contains(jugador) == true
+    partido.jugadoresConfirmados.contains(jugador) == true
+  }
+
+  void AltaJugadorSinPenalizacionesAPartidoSinConfirmar() {
+    given:'Como jugador de un grupo de amigos quiero anotarme para jugar un partido para participar del mismo.'
+    GrupoDeAmigos grupo = new GrupoDeAmigos(nombre:'F10')
+    Jugador jugador = new Jugador()
+    grupo.agregarAmigo(jugador)
+    Cancha cancha = new Cancha(numero: 5)
+    Partido partido = new Casual(new Date(), cancha)
+
+    when:'Me anoto'
+    partido.anotarJugador(jugador)
+
+    then:'Quedo como jugador anotado en el partido'
+    partido.jugadoresAnotados.contains(jugador) == true
+
+    when:'No confirmo'
+    then:'Quedo penalizado'
+    if (!partido.jugadoresConfirmados.contains(jugador)) {
+      jugador.penalizar()
+    }
+
+    jugador.reputacion.tieneBuenaReputacion() == false
+  }
+
+  void AltaJugadorConPenalizacionesAPartido() {
+    given:'Como jugador, con penalizaciones, de un grupo de amigos quiero anotarme para jugar un partido para participar del mismo.'
+    GrupoDeAmigos grupo = new GrupoDeAmigos(nombre:'F10')
+    Jugador jugador = new Jugador()
+    jugador.penalizar()
+    grupo.agregarAmigo(jugador)
+    Cancha cancha = new Cancha(numero: 5)
+    Partido partido = new Casual(new Date(), cancha)
+
+    when:'Me anoto'
+    partido.anotarJugador(jugador)
+
+    then:'No quedo como anotado ni confirmado'
+    partido.jugadoresAnotados.contains(jugador) == false
+    partido.jugadoresConfirmados.contains(jugador) == false
+  }
+
+}
